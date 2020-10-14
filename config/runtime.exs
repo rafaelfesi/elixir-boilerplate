9dbe75de47622ef37afb939cf85d2bf49ef5748b
Guillaume Cauchon <gcauchon@gmail.com>
2020-10-13 21:54:52 -0400

    Adapt the Environment module to the new config/runtime.exs setup

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,29 +1,8 @@
 import Config
 
-defmodule Environment do
-  @moduledoc """
-  This modules provides various helpers to handle environment metadata
-  """
-
-  def get(key), do: System.get_env(key)
-
-  def get_boolean(key) do
-    case get(key) do
-      "true" -> true
-      "1" -> true
-      _ -> false
-    end
-  end
-
-  def get_integer(key) do
-    case get(key) do
-      value when is_bitstring(value) -> String.to_integer(value)
-      _ -> nil
-    end
-  end
-
+defmodule RuntimeEnvironment do
   def get_cors_origins do
-    case get("CORS_ALLOWED_ORIGINS") do
+    case Environment.get("CORS_ALLOWED_ORIGINS") do
       origins when is_bitstring(origins) ->
         origins
         |> String.split(",")
@@ -38,12 +17,12 @@ defmodule Environment do
   end
 
   def get_endpoint_static_url_config do
-    case get("STATIC_URL_HOST") do
+    case Environment.get("STATIC_URL_HOST") do
       host when is_bitstring(host) ->
         [
           host: host,
-          scheme: get("STATIC_URL_SCHEME"),
-          port: get("STATIC_URL_PORT")
+          scheme: Environment.get("STATIC_URL_SCHEME"),
+          port: Environment.get("STATIC_URL_PORT")
         ]
 
       _ ->
@@ -70,14 +49,14 @@ config :elixir_boilerplate, ElixirBoilerplateWeb.Endpoint,
   debug_errors: Environment.get_boolean("DEBUG_ERRORS"),
   http: [port: port],
   secret_key_base: Environment.get("SECRET_KEY_BASE"),
-  static_url: Environment.get_endpoint_static_url_config(),
+  static_url: RuntimeEnvironment.get_endpoint_static_url_config(),
   url: [host: host, scheme: scheme, port: port]
 
 config :elixir_boilerplate, ElixirBoilerplateWeb.Router,
   session_key: Environment.get("SESSION_KEY"),
   session_signing_salt: Environment.get("SESSION_SIGNING_SALT")
 
-config :elixir_boilerplate, Corsica, origins: Environment.get_cors_origins()
+config :elixir_boilerplate, Corsica, origins: RuntimeEnvironment.get_cors_origins()
 
 config :elixir_boilerplate,
   basic_auth: [
